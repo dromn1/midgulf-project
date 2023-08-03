338bbdd5fd3286b316c78df0c367d3d4bc08974d
dromn1 <nhamode250@gmail.com>
2023-08-03 07:08:00 +0300

    our product update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ShariyahComponent } from './shariyah/shariyah.component';
 import { InvestorComponent } from './investor/investor.component';
+import { HealthComponent } from './health/health.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { InvestorComponent } from './investor/investor.component';
     AboutComponent,
     ShariyahComponent,
     InvestorComponent,
+    HealthComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/health/health.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-health',
+  templateUrl: './health.component.html',
+  styleUrls: ['./health.component.css']
+})
+export class HealthComponent {
+  health_img = "../../assets/health_img.png";
+  icon_medgulf = "../../assets/icon_medgulf.png";
+  // icons
+  icon_ph = "../../assets/icon_ph.svg";
+  icon_wo = "../../assets/icon_wo.svg";
+  icon_pen = "../../assets/icon_pen.svg";
+  icon_sp = "../../assets/icon_sp.svg";
+  icon_hos = "../../assets/icon_hos.svg";
+
+   i1= "../../assets/i1.svg";
+   i2= "../../assets/i2.svg";
+   i3= "../../assets/i3.svg";
+   i4= "../../assets/i4.svg";
+   i5= "../../assets/i5.svg";
+   i6= "../../assets/i6.svg";
+   i7= "../../assets/i7.svg";
+   i8= "../../assets/i8.svg";
+
+}
